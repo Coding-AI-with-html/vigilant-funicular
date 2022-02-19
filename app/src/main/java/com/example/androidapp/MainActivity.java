214dac88fdf0ae7556df6afcfd9781dc9c5dd944
Coding-AI-with-html <mankuspijus@gmail.com>
2022-02-19 22:34:52 +0200

    Error with mysql code by recognizing user

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -4,14 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.androidapp.common.LoginSignup.SignUp2ndClass;
+import com.example.androidapp.common.LoginSignup.User;
+import com.google.gson.Gson;
+import com.vishnusivadas.advanced_httpurlconnection.FetchData;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView usrName,usrEmail,usrGender;
+
+
+    private static final String TAG = "MainActivity";
+
+    private List<User> userInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +73,10 @@ public class MainActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
 
+        usrName = findViewById(R.id.username_logged);
+        usrEmail = findViewById(R.id.email_logged);
+        usrGender = findViewById(R.id.character_logged);
+        retrieveData();
     }
 
     public static void setWindowFlag(Activity activity, final int bits, boolean on) {
@@ -46,5 +89,66 @@ public class MainActivity extends AppCompatActivity {
             winParams.flags &= ~bits;
         }
         win.setAttributes(winParams);
+    }
+
+    private void retrieveData() {
+
+        String url = "http://192.168.122.1/LoginRegister/retrievedata.php";
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject responseJson = new JSONObject(response);
+                            String status = responseJson.getString("status");
+                            String message = responseJson.getString("message");
+
+                            if (status.contains("error")) {
+                                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                            } else {
+                                User userModel = new Gson().fromJson(responseJson.getString("username"), User.class);
+                                usrName.setText(userModel.getUsername());
+                                usrEmail.setText(userModel.getEmail());
+                                usrGender.setText(userModel.getGender());
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(getApplicationContext(), error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("id", "1");
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+// To prevent timeout error
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(50000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+// Add the request to the RequestQueue.
+        stringRequest.setShouldCache(false);
+        queue.add(stringRequest);
     }
 }
---
+++ app/src/main/java/com/example/androidapp/common/LoginSignup/User.java
@@ -0,0 +1,45 @@
+package com.example.androidapp.common.LoginSignup;
+
+public class User {
+
+    private String id,username, email, gender;
+
+
+    public User(String username, String email, String gender) {
+        this.username = username;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
